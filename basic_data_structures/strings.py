9ebbf945ba04198575a9689af089c528fff0a672
tanayakarmakar <karmakar.tanaya@gmail.com>
2025-05-09 20:43:58 +0530

    code

---
+++ basic_data_structures/strings.py
@@ -0,0 +1,8 @@
+my_name = 'Tanaya Karmakar'
+first_char = my_name[0]
+
+print(f"First character of {my_name} is {first_char}")
+
+last_char = my_name[-1]
+
+print(f"First character of {my_name} is {last_char}")
